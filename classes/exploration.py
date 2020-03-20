2612e78a1fcc99b5701cf1dd7b02568355ed29cc
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-03-20 09:08:04 +0100

    Add animals, add a fight system (unifinished)

--- classes/exploration.py
+++ classes/exploration.py
@@ -12,14 +12,12 @@ class Explore:
             continue_exploration = input('Do you want to continue ? (yes/no)\n')
             if continue_exploration == 'no':
                 is_exploring = False
-            import pdb
-            pdb.set_trace()
             if is_exploring:
                 if when_exploring == 1:
                     print('Oh no! You does the malin and felt in the ravin. You loose 25LP.')
                     self.life -= 15
                 elif when_exploring == 2:
-                    animals = ['Rat', 'Crocodile', 'Bull']
+                    animals = ['Rat', 'Crocodile', 'Bull', 'Wolf', 'Cow', 'Dog', 'Cat', 'Coronavirus']
                     print('You\'re in front of a big and mysterious animal...\n')
                     time.sleep(3)
                     print('...we can distinguish it\'s...')
@@ -31,9 +29,22 @@ class Explore:
 
                     function.to_lower(choices, choices_lower)
 
-                    self_choice = input('What do you do ?\n'
-                                          'Fight\n'
-                                          'Run')
+                    player_choice = input('What do you do ?\n'
+                                        '   - Fight\n'
+                                        '   - Run\n')
+                    if player_choice == choices_lower[0]:
+                        if self.xp < 50:
+                            print('You have 10% of chance to win.\n')
+                            continue_fight = input('Are you sure you want to continue ?\n')
+                            issue = ['win', 'loose']
+                            chances = []
+                            if continue_fight == 'yes':
+                                chances = [0.1, 0.9]
+                                result = random.choice(issue)
+                                import pdb
+                                pdb.set_trace()
+                            elif continue_fight == 'no':
+                                print('ok')
                 elif when_exploring == 3:
                     print('3')
 
