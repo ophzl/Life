423192fb3753364bca2cf9d7063833345f475b2c
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-03-20 10:37:25 +0100

    Improve Explore gameplay

--- classes/exploration.py
+++ classes/exploration.py
@@ -1,5 +1,6 @@
 import time, random
 
+
 class Explore:
     is_exploring = False
 
@@ -14,15 +15,15 @@ class Explore:
                 is_exploring = False
             if is_exploring:
                 if when_exploring == 1:
-                    print('Oh no! You does the malin and felt in the ravin. You loose 25LP.')
+                    print('(-25LP) || Oh no! Who does the malin fall in the ravin. You loose 25LP.')
                     self.life -= 15
                 elif when_exploring == 2:
                     animals = ['Rat', 'Crocodile', 'Bull', 'Wolf', 'Cow', 'Dog', 'Cat', 'Coronavirus']
-                    print('You\'re in front of a big and mysterious animal...\n')
+                    print('\nYou\'re in front of a big and mysterious animal...')
                     time.sleep(3)
                     print('...we can distinguish it\'s...')
                     time.sleep(1)
-                    print('...a ' + random.choice(animals) + '!')
+                    print('...a ' + random.choice(animals) + '!\n')
                     time.sleep(1)
                     choices = ['Fight', 'Run']
                     choices_lower = []
@@ -30,21 +31,24 @@ class Explore:
                     function.to_lower(choices, choices_lower)
 
                     player_choice = input('What do you do ?\n'
-                                        '   - Fight\n'
-                                        '   - Run\n')
+                                          '   - Fight\n'
+                                          '   - Run\n')
                     if player_choice == choices_lower[0]:
                         if self.xp < 50:
-                            print('You have 10% of chance to win.\n')
-                            continue_fight = input('Are you sure you want to continue ?\n')
+                            print('\nYou have 10% of chance to win.\n')
+                            continue_fight = input('Are you sure you want to continue ? (yes/no)\n')
                             issue = ['win', 'loose']
-                            chances = []
-                            if continue_fight == 'yes':
+                            if continue_fight != 'no':
                                 chances = [0.1, 0.9]
-                                result = random.choice(issue)
-                                import pdb
-                                pdb.set_trace()
+                                result = random.choices(issue, chances)
+                                if result == ['win']:
+                                    print('(+15px) || You\'re really lucky. You win this fight and gain 15xp.')
+                                    self.xp += 15
+                                if result == ['loose']:
+                                    print('(-25LP) || You loose... ')
                             elif continue_fight == 'no':
                                 print('ok')
+
                 elif when_exploring == 3:
                     print('3')
 
