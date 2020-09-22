82478a96509e4a936749f37d93b3ed2966d73a6b
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-09-22 14:50:05 +0200

    Improve food system

--- classes/food.py
+++ classes/food.py
@@ -1,15 +1,15 @@
 import json
+from . import Functions
 
 class Feed:
-    # TODO: implement a food system
-    def feed(self, game_save):
-        with open('txt/saves/save'+ game_save +'.json', 'r') as json_file:
-            json_data = json.load(json_file)
-
-        if json_data['food']:
+    def feed(self):
+        search = Functions.search_action('Meat', self.inventory)
+        if search and self.foodPoints < 100:
             print(_('(+50FP) What a good feeling...'))
-            json_data['food'] -= 50
-            json_data['foodPoints'] += 50
-
-        else:
+            self.foodPoints += 50
+            self.inventory.pop()
+        elif not search:
             print(_('You don\'t have enough food !'))
+
+        elif self.foodPoints == 100:
+            print(_('Vous n\'avez pas besoin de manger.'))
