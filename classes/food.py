89475e727aa7acfa2920f7a59b565f52f5536462
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-09-22 10:18:32 +0200

    Add Feed class

--- classes/food.py
+++ classes/food.py
@@ -1,4 +1,15 @@
+import json
+
 class Feed:
-    food = 0 # TODO: implement a food system
-    def feed(self):
-        print('Yummy!')
+    # TODO: implement a food system
+    def feed(self, game_save):
+        with open('txt/saves/save'+ game_save +'.json', 'r') as json_file:
+            json_data = json.load(json_file)
+
+        if json_data['food']:
+            print(_('(+50FP) What a good feeling...'))
+            json_data['food'] -= 50
+            json_data['foodPoints'] += 50
+
+        else:
+            print(_('You don\'t have enough food !'))
