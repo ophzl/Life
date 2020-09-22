011dbb040b18ab217d439d1e169c8d084041e652
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-09-22 14:50:57 +0200

    Improve Player class to stock player's data

--- classes/player.py
+++ classes/player.py
@@ -4,12 +4,9 @@ class Player:
     name = ''
     life = 0
     xp = 0
+    inventory = []
+    foodPoints = 0
     money = 0 # TODO
     # clothes = ['top', 'jeans', 'hat', 'shoes']  # TODO: implement a weather system
     # tools = ['gun'] # TODO
     # food_stock = {'berries': 0} # TODO: Fix
-
-    def player(self):
-        with open('txt/saves/save' + self + '.json', 'r') as json_file:
-            json_data = json.load(json_file)
-            return json_data
